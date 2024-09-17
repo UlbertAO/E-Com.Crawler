d4b099e785cc90cd38e32b2e542da0f215341708
ulbertAO <sp595993@gmail.com>
2024-09-18 00:58:16 +0530

    html download succss blob upload success

--- E-Com.Crawler/Function1.cs
+++ E-Com.Crawler/Function1.cs
@@ -1,5 +1,5 @@
-using System;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace E_Com.Crawler
@@ -7,21 +7,75 @@ namespace E_Com.Crawler
     public class Function1
     {
         private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly StorageManager _storageManager;
 
-        public Function1(ILoggerFactory loggerFactory)
+        public Function1(IConfiguration configuration, ILoggerFactory loggerFactory, StorageManager storageManager)
         {
+            _configuration = configuration;
             _logger = loggerFactory.CreateLogger<Function1>();
+            _storageManager = storageManager;
         }
 
         [Function("Function1")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 0 6 * * *", RunOnStartup = true)] TimerInfo timer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            
-            if (myTimer.ScheduleStatus is not null)
+            try
             {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+                await _storageManager.CreateContainer();
+                if (_storageManager._containerClient == null)
+                {
+                    throw new Exception("Container not created");
+                }
+
+                var urls = Environment.GetEnvironmentVariable("Ecom_Urls")?.Split(';');
+                if (urls == null)
+                {
+                    _logger.LogError("No eCommerce URLs found in environment variables.");
+                    return;
+                }
+                var productHtmls = new List<string>();
+                foreach (var url in urls)
+                {
+                    try
+                    {
+                        using (var httpClient = new HttpClient())
+                        {
+                            var response = await httpClient.GetAsync(url);
+                            response.EnsureSuccessStatusCode();
+
+                            var html = await response.Content.ReadAsStringAsync();
+
+                            productHtmls.Add(html);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Error scraping URL {url}: {ex.Message}");
+                    }
+                }
+
+                var count = 0;
+                foreach (var html in productHtmls) { 
+                    await _storageManager.UploadBlob($"html_{count}", html);
+                    count++;
+                }
+
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+            }
+            finally
+            {
+                if (timer.ScheduleStatus is not null)
+                {
+                    _logger.LogInformation($"Next timer schedule at: {timer.ScheduleStatus.Next}");
+                }
+            }
+
         }
     }
 }
---
+++ E-Com.Crawler/StorageManager.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging;
+
+
+namespace E_Com.Crawler
+{
+    public class StorageManager
+    {
+        private readonly ILogger _logger;
+        private BlobServiceClient _blobServiceClient;
+        public BlobContainerClient _containerClient;
+        public StorageManager(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<StorageManager>();
+
+            var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            if (connectionString == null)
+            {
+                _logger.LogError("No AzureWebJobsStorage connection string found in environment variables.");
+                return;
+            }
+            _blobServiceClient = new BlobServiceClient(connectionString);
+        }
+        public async Task CreateContainer()
+        {
+            var containerName = Environment.GetEnvironmentVariable("ContainerName");
+            if (containerName == null)
+            {
+                _logger.LogError("No containerName found in environment variables.");
+                return;
+            }
+            _containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await _containerClient.CreateIfNotExistsAsync();
+        }
+        public async Task UploadBlob(string productName, string htmlContent)
+        {
+            var currentDate = DateTime.Now.ToString("yyyy/MM/dd");
+            var blobName = $"{currentDate}/{productName}.html";
+
+            BlobClient blobClient = _containerClient.GetBlobClient(blobName);
+
+
+            using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(htmlContent)))
+            {
+                await blobClient.UploadAsync(stream, overwrite: true);
+                _logger.LogInformation($"Uploaded HTML to blob storage.");
+            }
+        }
+
+    }
+}
